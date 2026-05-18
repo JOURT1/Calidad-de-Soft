a444e382e25c2ae255536d5233ea2875fb1cdeb0
Mauro0407 <130101420+Mauro0407@users.noreply.github.com>
2026-05-17 21:32:22 -0500

    Add files via upload

---
+++ Calidad-de-Soft-main/src/test/java/GestorPuntuacionTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GestorPuntuacionTest {
+
+    @Test
+    void debeSumarPuntosAUnJugador() {
+        GestorPuntuacion gestor = new GestorPuntuacion();
+
+        gestor.sumarPuntos("Justin", 10);
+
+        assertEquals(10, gestor.obtenerPuntos("Justin"));
+    }
+
+    @Test
+    void debeAcumularPuntosDelMismoJugador() {
+        GestorPuntuacion gestor = new GestorPuntuacion();
+
+        gestor.sumarPuntos("Justin", 10);
+        gestor.sumarPuntos("Justin", 5);
+
+        assertEquals(15, gestor.obtenerPuntos("Justin"));
+    }
+
+    // --- Cycle 2 ---
+    @Test
+    void debeRestarPuntosAUnJugador() {
+        GestorPuntuacion gestor = new GestorPuntuacion();
+        gestor.sumarPuntos("Stefan", 20);
+        
+        gestor.restarPuntos("Stefan", 5);
+        
+        assertEquals(15, gestor.obtenerPuntos("Stefan"));
+    }
+
+        // --- Cycle 3 ---
+    @Test
+    void debeConsultarLaPuntuacionActualDeUnJugador() {
+        GestorPuntuacion gestor = new GestorPuntuacion();
+        gestor.sumarPuntos("Mauricio", 50);
+
+        assertEquals(50, gestor.obtenerPuntos("Mauricio"));
+    }
+
+    // --- Cycle 4 ---
+    @Test
+    void debeReiniciarLosPuntosDeUnJugadorA_cero() {
+        GestorPuntuacion gestor = new GestorPuntuacion();
+        gestor.sumarPuntos("Jhoel", 20);
+
+        gestor.reiniciarPuntos("Jhoel");
+
+        assertEquals(0, gestor.obtenerPuntos("Jhoel"));
+    }
+}
